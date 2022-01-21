461e69bd5f88cf67a7ea133d53d1b3a2236fc44c
Jose Ramirez <josemramirez81@outlook.com>
2022-01-21 18:15:10 -0500

    Refactor config variables

--- config/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-
-}
-
-export default config
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import config from './config/config'
+import environment from './config/environment'
 import app from './express'
 
 
-app.listen(config.port, (err) => {
+app.listen(environment.port, (err) => {
     if(err){
         console.log(err)
     }
-    console.info('Server started on port %s', config.port)
+    console.info('Server started on port %s', environment.port)
 })
