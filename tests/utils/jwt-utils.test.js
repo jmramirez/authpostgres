abb29cfd8f818015de5160a60519f1fc290b2067
Jose Ramirez <josemramirez81@outlook.com>
2022-01-21 19:00:30 -0500

    Define tests methods for jwt methods

---
+++ tests/utils/jwt-utils.test.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken'
+import JWTUtils from "../../utils/jwt.utils";
+
+describe('jwt utils', () => {
+    it('should return an access token', () => {
+        const payload = { email: 'test@example.com'}
+        expect(JWTUtils.generateAccessToken(payload)).toEqual(expect.any(String))
+    })
+
+    it('should return a refresh token', () => {
+        const payload = { email: 'test@example.com'}
+        expect(JWTUtils.generateRefreshToken(payload)).toEqual(expect.any(String))
+    })
+
+    it('should verify that the access token is valid', () => {
+        const payload = { email: 'test@example.com'}
+        const jwt = JWTUtils.generateAccessToken(payload)
+        expect(JWTUtils.verifyAccessToken(jwt)).toEqual(
+            expect.objectContaining(payload)
+        )
+    })
+
+    it('should verify that the refresh token is valid', () => {
+        const payload = { email: 'test@example.com'}
+        const jwt = JWTUtils.generateRefreshToken(payload)
+        expect(JWTUtils.verifyRefreshToken(jwt)).toEqual(
+            expect.objectContaining(payload)
+        )
+    })
+
+    it('should break if the access token is invalid', () => {
+        expect(() => JWTUtils.verifyAccessToken('invalidToken')).toThrow(
+            jwt.JsonWebTokenError
+        )
+    })
+
+    it('should break if the refresh token is invalid', () => {
+        expect(() => JWTUtils.verifyRefreshToken('invalidToken')).toThrow(
+            jwt.JsonWebTokenError
+        )
+    })
+})
