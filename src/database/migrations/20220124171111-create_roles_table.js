9c957b7010cc867b4066a5624783979da9ebcc51
Jose Ramirez <josemramirez81@outlook.com>
2022-01-24 12:50:58 -0500

    Create all database migrations

---
+++ src/database/migrations/20220124171111-create_roles_table.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: { model: {tableName: 'Users'}, key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Roles')
+  }
+};
